a9e95dc3cece550a99a5de87ae33ce428d9fbb3a
GeorginaVillarojo-UCM <georginavillarojo19@gmail.com>
2025-08-31 04:02:09 -0700

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,18 @@
+public class Comments{
+
+   public static void main(String[] args){
+   
+   //This is a Single-line Comment
+   System.out.println("Hello World!");
+   
+   /* This is a Multi-line Comment */
+   System.out.println("Oh, ma douce souffrance");
+   
+   /**
+   *This is a Documentation Comment
+   *I'll input my fav line song as an example
+   */
+   System.out.println("naega deo jalhalgeyo, ileohge gat-i iss-eojundamyeon");
+   
+   }
+   }
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Tell your friends that I love the drama");
+        System.out.println("Play pretend, but you know it's karma");
+        System.out.println("You're the one that's lonely callin'");
+        System.out.println("You can blame me, but I ain't the problem (ooh)");
+    }
+}
