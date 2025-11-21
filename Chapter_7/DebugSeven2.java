715ade9414aa4cc7dad342f6fd5f95e81530ff23
GeorginaVillarojo-UCM <georginavillarojo19@gmail.com>
2025-11-21 08:12:10 +0800

    Add files via upload

---
+++ Chapter_7/DebugSeven2.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+class DebugSeven2
+{
+   public static void main(String[] a)
+   {
+      String str;
+      int x;
+      int length;
+      int num;
+      int lastSpace = -1;
+      int sum = 0;
+      String partStr;
+
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter a series of integers separated by spaces >> ");
+      str = in.nextLine();
+      length = str.length();
+
+      for(x = 0; x < length; ++x)  
+      {
+         if(str.charAt(x) == ' ')
+         {
+            partStr = str.substring(lastSpace + 1, x);
+            num = Integer.parseInt(partStr);
+            System.out.println(" " + num);
+            sum += num;
+            lastSpace = x;
+         }
+      }
+
+     
+      partStr = str.substring(lastSpace + 1, length);
+      num = Integer.parseInt(partStr);
+      System.out.println(" " + num);
+      sum += num;  
+
+      System.out.println("-------------------" +
+         "\nThe sum of the integers is " + sum);
+   }
+}
